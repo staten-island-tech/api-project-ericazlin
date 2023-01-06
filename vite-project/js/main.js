a2f71d4efd2d0d20d2af3bc98bd22c15ff0418da
ericazlin <113201249+ericazlin@users.noreply.github.com>
2023-01-06 13:13:51 -0500

    english test was horrible gonna cry

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,17 +1,32 @@
 import '../styles/style.css'
-
 const URL = "https://meowfacts.herokuapp.com/"
+booty(URL);
 
-async function booty(URL){
+async function booty(URL) {
   try {
-    const response = await fetch(URL);
-    const data = await response.json();
-    data.data.forEach(text => {
-      document.body.textContent = text
-    });
+      const response = await fetch(URL);
+      const data = await response.json();
+      console.log(data);
+      //accounts for 1 and 2 part jokes
+      if (data.setup) {
+          DOMSelectors.text.innerHTML = `${data.setup} <br><br> ${data.delivery}`;
+      } else {
+          DOMSelectors.text.innerHTML = data.text;
+      }
+
+      console.log(response);
+      return data;
   } catch (error) {
-    console.log(error)
+      console.log(error);
   }
 }
-
 booty(URL);
+
+
+// async function booty(URL) {
+//   try {
+//     const response = await fetch(URL);
+//     const data = await response.json();
+//     data.data.forEach(text => {
+//       document.body.textContent = text
+//     });
