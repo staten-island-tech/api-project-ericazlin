6afb5cf1badf93755af425e1ae007ca7439b742d
ericazlin <113201249+ericazlin@users.noreply.github.com>
2023-01-09 13:14:28 -0500

    russian test in tywo periods yay

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,17 +2,12 @@ import '../styles/style.css'
 const URL = "https://meowfacts.herokuapp.com/"
 booty(URL);
 
+document.querySelector(".btn").addEventListener("click", booty)
 async function booty(URL) {
   try {
       const response = await fetch(URL);
       const data = await response.json();
       console.log(data);
-      //accounts for 1 and 2 part jokes
-      if (data.setup) {
-          DOMSelectors.text.innerHTML = `${data.setup} <br><br> ${data.delivery}`;
-      } else {
-          DOMSelectors.text.innerHTML = data.text;
-      }
 
       console.log(response);
       return data;
