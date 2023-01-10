017481f8a175316768a0652341c8b6f09902f65c
ericazlin <113201249+ericazlin@users.noreply.github.com>
2023-01-10 13:13:58 -0500

    wowowowow i did more than usual today yay

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,27 +1,15 @@
 import '../styles/style.css'
 const URL = "https://meowfacts.herokuapp.com/"
-booty(URL);
 
-document.querySelector(".btn").addEventListener("click", booty)
 async function booty(URL) {
   try {
       const response = await fetch(URL);
       const data = await response.json();
       console.log(data);
-
-      console.log(response);
-      return data;
   } catch (error) {
       console.log(error);
   }
 }
-booty(URL);
 
+document.querySelector(".btn").addEventListener("click", booty(URL))
 
-// async function booty(URL) {
-//   try {
-//     const response = await fetch(URL);
-//     const data = await response.json();
-//     data.data.forEach(text => {
-//       document.body.textContent = text
-//     });
