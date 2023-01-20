b1c500a1723f10a466da4e9ce0530afa3cbcaf60
ericazlin <113201249+ericazlin@users.noreply.github.com>
2023-01-20 13:11:27 -0500

    gonna try using my vite code for this cause umm also just took physics multiple choice today absolutely threw but at least i guessed some of them right umm

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,15 +1,64 @@
-import '../styles/style.css'
-const URL = "https://meowfacts.herokuapp.com/"
+// import '../styles/style.css'
+// const URL = "https://meowfacts.herokuapp.com/"
 
-async function booty(URL) {
-  try {
-      const response = await fetch(URL);
-      const data = await response.json();
-      console.log(data);
-  } catch (error) {
-      console.log(error);
-  }
-}
+// async function booty(URL) {
+//   try {
+//       const response = await fetch(URL);
+//       const data = await response.json();
+//       console.log(data);
+//   } catch (error) {
+//       console.log(error);
+//   }
+// }
                    
-document.querySelector(".btn").addEventListener("click", booty(URL))
+// document.querySelector(".btn").addEventListener("click", booty(URL))
+//https://github.com/alexwohlbruck/cat-facts
+import '../styles/style.css'
+import {products} from './products.js'
 
+const DOMSelectors = {
+    everything: document.querySelector(".everything"),
+    btn: document.querySelector(".btn"),
+    filter: document.getElementById("filter"),
+        doofBtn: document.getElementById("doofBtn"),
+        existBtn: document.getElementById("existBtn"),
+        frozenBtn: document.getElementById("frozenBtn"),
+        snackBtn: document.getElementById("snackBtn"),
+        freshBtn: document.getElementById("freshBtn"),
+        chemicalsBtn: document.getElementById("chemicalsBtn"),
+        hotFoodBtn: document.getElementById("hotFoodBtn"),
+        carPartsBtn: document.getElementById("carPartsBtn"),
+        drinksBtn: document.getElementById("drinksBtn"),
+        coldFoodBtn: document.getElementById("coldFoodBtn"),
+        candyBtn: document.getElementById("candyBtn"),
+        bakedBtn: document.getElementById("bakedBtn"),
+}
+
+function filteringBool(productAttr) {
+
+    const productsFiltered = products.filter(product => product[productAttr] == true)
+
+    productsFiltered.forEach(card => {
+        DOMSelectors.everything.insertAdjacentHTML("afterbegin",
+                                                  `
+      <div id=card>
+      <img src=${card.image}>
+      <h1 id="text"> this is ${card.name} </h1>
+      </img>
+      </div>
+      `
+      )
+    })
+    console.log(productsFiltered)
+}
+
+function clearProducts() {
+    DOMSelectors.everything.innerHTML = ""
+}
+
+filteringBool("exists")
+
+DOMSelectors.existBtn.addEventListener("click", function () {
+    clearProducts()
+    filteringBool("exists")
+})
