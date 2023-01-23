0ebc0fa56901234fe0eb2e861571376f0225bc41
ericazlin <ericazlin@gmail.com>
2023-01-23 12:40:18 -0500

    djasuifaijsbn

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,15 +1,31 @@
 import '../styles/style.css'
 const URL = "https://meowfacts.herokuapp.com/"
 
+const DOMSelectors = {
+  factBtn: document.getElementById("factBtn"),
+  fact: document.querySelector(".fact"),
+}
+
 async function booty(URL) {
   try {
       const response = await fetch(URL);
       const data = await response.json();
-      console.log(data);
+      DOMSelectors.fact.insertAdjacentText('afterbegin', `${data.data}`)
+      console.log(data.data)
   } catch (error) {
       console.log(error);
   }
+}       
+
+function clear() {
+  DOMSelectors.fact.innerHTML = ""
 }
- m                        
-document.querySelector(".btn").addEventListener("click", booty(URL))
+
+booty(URL)
+
+DOMSelectors.factBtn.addEventListener("click", function() {
+  clear()
+  booty(URL)
+
+})
 
